5c2e145b2995ee88258e3a684be853684170bdbc
hashtechgermany <72353541+hashtechgermany@users.noreply.github.com>
2021-01-04 20:33:59 +0100

    Stack Navigation 03

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Button, View, Text} from 'react-native';
 
-const DetailsScreen = ({navigation}) => {
+const DetailsScreen = ({navigation, route}) => {
+  const {itemId, otherParam} = route.params;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
       />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
