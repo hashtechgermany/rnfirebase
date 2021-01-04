e35df2bf1d9155810d1d1be39b9eac1507b22f53
hashtechgermany <72353541+hashtechgermany@users.noreply.github.com>
2021-01-04 22:32:48 +0100

    Satck Navigator 05 Passing Params

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {View, Text} from 'react-native';
 
-const HomeScreen = ({navigation, route}) => {
-  React.useEffect(() => {
-    if (route.params?.post) {
-      // Post updated, do something with `route.params.post`
-      // For example, send the post to the server
-    }
-  }, [route.params?.post]);
-
+const HomeScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        title="Create post"
-        onPress={() => navigation.navigate('CreatePost')}
-      />
-      <Text style={{margin: 10}}>Post: {route.params?.post}</Text>
+      <Text>Home Screen</Text>
     </View>
   );
 };
