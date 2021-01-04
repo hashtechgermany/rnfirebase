96d2c90353b10c0241ddc3e316422b548d87eedf
hashtechgermany <72353541+hashtechgermany@users.noreply.github.com>
2021-01-04 22:29:48 +0100

    Satck Navigator 05 Passing Params

--- App.js
+++ App.js
@@ -5,15 +5,19 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import CreatePostScreen from './screens/CreatePostScreen';
+import LogoTitle from './screens/LogoTitle';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator mode="modal">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerTitle: (props) => <LogoTitle {...props} />}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
